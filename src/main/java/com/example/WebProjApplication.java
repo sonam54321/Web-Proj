90a8932fada8efa814ad0deca59b17ed62c0a763
Snehal <sonamramteke34@gmail.com>
2022-11-13 00:19:54 +0530

    Added

---
+++ src/main/java/com/example/WebProjApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProjApplication.class, args);
+	}
+
+}
